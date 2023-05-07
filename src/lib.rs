ad9498e773424b40826bf034d9f4642b5e7330ee
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2023-05-07 22:38:20 +0200

    The meta frontier thing seems to be a bad idea

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,10 @@
 mod frontier;
-mod meta_frontier;
 mod iter;
 mod par_iter;
 mod par_iter_indexed;
 
 pub mod prelude {
     pub use crate::frontier::*;
-    pub use crate::meta_frontier::*;
     pub use crate::iter::*;
     pub use crate::par_iter::*;
     pub use crate::par_iter_indexed::*;
